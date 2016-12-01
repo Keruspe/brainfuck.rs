c8154f1cd5fda9a3d89f5be46844bbca2790d548
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2016-12-01 23:34:59 +0100

    ast::Token is dead
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/ast.rs
+++ src/ast.rs
@@ -4,34 +4,6 @@ use std::convert::From;
 use std::iter::IntoIterator;
 
 #[derive(Debug)]
-pub enum Token {
-    LShift,
-    RShift,
-    Plus,
-    Minus,
-    Dot,
-    Comma,
-    LBracket,
-    RBracket,
-}
-
-impl Token {
-    pub fn from_char(ch: char) -> Option<Token> {
-        match ch {
-            '<' => Some(Token::LShift),
-            '>' => Some(Token::RShift),
-            '+' => Some(Token::Plus),
-            '-' => Some(Token::Minus),
-            '.' => Some(Token::Dot),
-            ',' => Some(Token::Comma),
-            '[' => Some(Token::LBracket),
-            ']' => Some(Token::RBracket),
-            _   => None,
-        }
-    }
-}
-
-#[derive(Debug)]
 pub enum Node {
     LShift,
     RShift,
