4041500dc50c2b8d55925fb6be921c3f7d50faec
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2017-01-23 21:22:03 +0100

    bf: better error handling
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/bin/bf.rs
+++ src/bin/bf.rs
@@ -4,11 +4,12 @@ extern crate clap;
 use brainfuck::context::Context;
 use brainfuck::parser;
 use clap::{App, Arg};
+use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 
 fn main() {
-    let matches     = App::new("Brainfuck interpreter")
+    let matches = App::new("Brainfuck interpreter")
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
         .about("Read, parse and execute a brainfuck program")
@@ -17,20 +18,17 @@ fn main() {
              .required(true)
              .index(1))
         .get_matches();
-    let input       = matches.value_of("INPUT").unwrap();
-    let mut file    = match File::open(input) {
-        Ok(file) => file,
-        Err(err) => panic!("Failed to open {}: {:?}", input, err),
-    };
-    let mut ctx      = Context::new();
+    let input   = matches.value_of("INPUT").unwrap();
+
+    if let Err(err) = run(input) {
+        println!("Error while running {}: {}", input, err);
+    }
+}
+
+fn run(input: &str) -> Result<(), Box<Error>> {
     let mut contents = vec![];
 
-    if let Err(err)  = file.read_to_end(&mut contents) {
-        panic!("Failed to read {}: {:?}", input, err);
-    }
+    File::open(input)?.read_to_end(&mut contents)?;
 
-    match parser::parse(contents.as_ref()) {
-        Ok(block) => ctx.run(&block),
-        Err(err)  => panic!("Failed to run {}: {:?}", input, err),
-    }
+    Ok(Context::new().run(&parser::parse(contents.as_ref())?))
 }
